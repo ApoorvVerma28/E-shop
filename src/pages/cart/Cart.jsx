71cbca8849c6297e8461eb7232b253705b42ce17
ApoorvVerma28 <rishuverma870735@gmail.com>
2024-07-26 00:30:58 +0530

    RazorPay Setup

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -24,6 +24,91 @@ const CartPage = () => {
   }, [cartItems]);
   const shipping = parseInt(100);
   const grandTotal = shipping + totalAmount;
+
+  const [name, setName] = useState("")
+  const [address, setAddress] = useState("");
+  const [pincode, setPincode] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+
+  const buyNow = async () => {
+    // validation 
+    if (name === "" || address == "" || pincode == "" || phoneNumber == "") {
+      return toast.error("All fields are required", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      })
+    }
+    const addressInfo = {
+      name,
+      address,
+      pincode,
+      phoneNumber,
+      date: new Date().toLocaleString(
+        "en-US",
+        {
+          month: "short",
+          day: "2-digit",
+          year: "numeric",
+        }
+      )
+    }
+    console.log(addressInfo)
+
+    var options = {
+      key: "rzp_test_fWMflLeZ9A8pLD",
+      key_secret: "nWvqCnxa8CdqeOOskwNN2v3o",
+      amount: parseInt(grandTotal * 100),
+      currency: "INR",
+      order_receipt: 'order_rcptid_' + name,
+      name: "E-Bharat",
+      description: "for testing purpose",
+      handler: function (response) {
+        // console.log(response)
+        toast.success('Payment Successful')
+        const paymentId = response.razorpay_payment_id
+        // store in firebase 
+        const orderInfo = {
+          cartItems,
+          addressInfo,
+          date: new Date().toLocaleString(
+            "en-US",
+            {
+              month: "short",
+              day: "2-digit",
+              year: "numeric",
+            }
+          ),
+          email: JSON.parse(localStorage.getItem("user")).user.email,
+          userid: JSON.parse(localStorage.getItem("user")).user.uid,
+          paymentId
+        }
+
+        try {
+          const result = addDoc(collection(fireDB, "orders"), orderInfo)
+        } catch (error) {
+          console.log(error)
+        }
+      },
+
+      theme: {
+        color: "#3399cc"
+      }
+
+
+    };
+    var pay = new window.Razorpay(options);
+    pay.open();
+    console.log(pay)
+  }
+
+
+
   return (
     <Layout>
       <div className="container mx-auto  max-w-7xl  lg:px-0 ">
@@ -119,19 +204,29 @@ const CartPage = () => {
                     <dt className="flex text-sm text-gray-800">
                       <span>Delivery Charges</span>
                     </dt>
-                <dd className="text-sm font-medium text-green-700">${shipping}</dd>
+                    <dd className="text-sm font-medium text-green-700">${shipping}</dd>
                   </div>
                   <div className="flex items-center justify-between border-y border-dashed py-4 ">
                     <dt className="text-base font-medium text-gray-900">
                       Total Amount
                     </dt>
                     <dd className="text-base font-medium text-gray-900">
-                     ${grandTotal}
+                      ${grandTotal}
                     </dd>
                   </div>
                 </dl>
                 <div className="px-2 pb-4 font-medium text-green-700">
-                  <Modal />
+                  <Modal
+                    name={name}
+                    address={address}
+                    pincode={pincode}
+                    phoneNumber={phoneNumber}
+                    setName={setName}
+                    setAddress={setAddress}
+                    setPincode={setPincode}
+                    setPhoneNumber={setPhoneNumber}
+                    buyNow={buyNow}
+                  />
                 </div>
               </div>
             </section>
